3677440bdf575329c5ea589f1e9983d7243c5693
Shtykin <shtykin@mail.ru>
2023-02-27 01:51:57 +0700

    разделил сборки на версии dev и staging

--- app/src/main/java/ru/eshtykin/acs_monitoring_admin/presentation/MainActivity.kt
+++ app/src/main/java/ru/eshtykin/acs_monitoring_admin/presentation/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
+import ru.eshtykin.acs_monitoring_admin.network.Constants
 import ru.eshtykin.acs_monitoring_admin.presentation.screen.login.LoginScreen
 import ru.eshtykin.acs_monitoring_admin.presentation.screen.login.LoginScreenState
 import ru.eshtykin.acs_monitoring_admin.presentation.ui.theme.Acs_monitoringadminTheme
