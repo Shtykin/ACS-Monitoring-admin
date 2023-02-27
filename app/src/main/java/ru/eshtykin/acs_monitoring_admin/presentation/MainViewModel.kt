146ef24eeed325c74b01a169bb1562330187ab63
Shtykin <shtykin@mail.ru>
2023-02-28 00:06:17 +0700

    основные функции viewModel

--- app/src/main/java/ru/eshtykin/acs_monitoring_admin/presentation/MainViewModel.kt
+++ app/src/main/java/ru/eshtykin/acs_monitoring_admin/presentation/MainViewModel.kt
@@ -8,10 +8,11 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
 import ru.eshtykin.acs_monitoring_admin.data.repository.RepositoryImpl
+import ru.eshtykin.acs_monitoring_admin.domain.Owner
 import ru.eshtykin.acs_monitoring_admin.domain.User
-import ru.eshtykin.acs_monitoring_admin.presentation.screen.details.DetailsScreen
 import ru.eshtykin.acs_monitoring_admin.presentation.screen.details.DetailsScreenState
 import ru.eshtykin.acs_monitoring_admin.presentation.screen.login.LoginScreenState
+import ru.eshtykin.acs_monitoring_admin.presentation.screen.users.UsersScreenState
 import ru.eshtykin.acs_monitoring_admin.presentation.state.ScreenState
 
 class MainViewModel(application: Application): AndroidViewModel(application) {
@@ -21,6 +22,9 @@ class MainViewModel(application: Application): AndroidViewModel(application) {
     private val _uiState = mutableStateOf<ScreenState>(ScreenState.LoginScreen(LoginScreenState.Loading))
     val uiState: State<ScreenState>
         get() = _uiState
+
+    var token = ""
+
     init {
         viewModelScope.launch {
             try {
@@ -38,6 +42,18 @@ class MainViewModel(application: Application): AndroidViewModel(application) {
 
     }
 
+    fun authenticateAdmin(login: String, password: String) {
+        _uiState.value = ScreenState.LoginScreen(LoginScreenState.Loading)
+        viewModelScope.launch {
+            try {
+                token = repository.authAdmin(login, password)
+                _uiState.value = ScreenState.LoginScreen(LoginScreenState.Authorized)
+            } catch (e: Exception) {
+                _uiState.value = ScreenState.LoginScreen(LoginScreenState.UnAuthorized(e.message))
+            }
+        }
+    }
+
     fun getUser(login: String){
         _uiState.value = ScreenState.DetailsScreen(DetailsScreenState.Loading)
         viewModelScope.launch {
@@ -49,4 +65,52 @@ class MainViewModel(application: Application): AndroidViewModel(application) {
             }
         }
     }
+
+    fun getAllUsers(){
+        _uiState.value = ScreenState.UsersScreen(UsersScreenState.Loading)
+        viewModelScope.launch {
+            try {
+                val users = repository.getAllUsers()
+                _uiState.value = ScreenState.UsersScreen(UsersScreenState.Users(users))
+            } catch (e: Exception) {
+                _uiState.value = ScreenState.UsersScreen(UsersScreenState.Error(e.message))
+            }
+        }
+    }
+
+    fun changeUserRole(user: User, role: String) {
+        viewModelScope.launch {
+            try {
+                val result = repository.changeRole(user.login, role)
+                if (result) {
+                    val newUser = user.copy(role = role)
+                    _uiState.value = ScreenState.DetailsScreen(DetailsScreenState.Details(newUser))
+                } else {
+                    throw Exception("Failed, result: $result")
+                }
+            } catch (e: Exception) {
+                _uiState.value = ScreenState.DetailsScreen(DetailsScreenState.Error(e.message))
+            }
+        }
+    }
+
+    fun addUserOwner(user: User, owner: Owner) {
+        viewModelScope.launch {
+            try {
+                val owner = repository.addOwner(user.login, owner.value)
+                if (owner.value.isNotEmpty()) {
+                    val owners = user.owners?.toMutableList() ?: mutableListOf()
+                    owners.add(Owner(owner.value))
+                    val newUser = user.copy(owners = owners)
+                    _uiState.value = ScreenState.DetailsScreen(DetailsScreenState.Details(newUser))
+                } else {
+                    throw Exception("Failed, owner: ${owner.value}")
+                }
+            } catch (e: Exception) {
+                _uiState.value = ScreenState.DetailsScreen(DetailsScreenState.Error(e.message))
+            }
+        }
+    }
+
+
 }
--- app/src/main/java/ru/eshtykin/acs_monitoring_admin/presentation/screen/login/LoginScreenState.kt
+++ app/src/main/java/ru/eshtykin/acs_monitoring_admin/presentation/screen/login/LoginScreenState.kt
@@ -1,7 +1,11 @@
 package ru.eshtykin.acs_monitoring_admin.presentation.screen.login
 
+import ru.eshtykin.acs_monitoring_admin.presentation.screen.details.DetailsScreenState
+
 sealed class LoginScreenState {
     object Loading: LoginScreenState()
     object Authorized: LoginScreenState()
-    object UnAuthorized: LoginScreenState()
+    data class UnAuthorized(
+        val message: String? = null
+    ): LoginScreenState()
 }
